c6ffe9003b1ba1e8e9eed506d62cc71c6a80b537
radio <radio.github@gmail.com>
2016-06-10 01:59:27 -0700

    Adds CSV DB Entries

---
+++ model/csv.rb
@@ -0,0 +1,29 @@
+require 'csv'
+
+require './tootor.rb'
+require './db.rb'
+
+CSV.foreach('tootors.csv', headers: true) do |row|
+  t = Tootor.new
+
+  t.is_tootor = row[0]
+  t.username = row[1]
+  t.seo_name= row[2]
+  t.email = row[3]
+  t.password = row[4]
+  t.name = row[5]
+  t.phone = row[6]
+  t.price = row[7]
+  t.street = row[8]
+  t.city = row[9]
+  t.state = row[10]
+  t.zip = row[11]
+  t.focus = row[12]
+  t.description = row[13]
+  t.picture = row[14]
+  t.video = row[15]
+
+  # p t
+
+  TootorsDb.insert t
+end
